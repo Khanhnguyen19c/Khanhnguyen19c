5fedcf71a5ff0cf8709df5c5483ac3a1b8093b50
Khanhdangiuu <khanhlunn224@gmail.com>
2021-09-01 18:46:12 +0700

    khanhnguyen

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,5 +237,5 @@ window.addEventListener("load", ()=>{
     document.querySelector(".preloader").classList.add("fade-out");
     setTimeout(()=>{
         document.querySelector(".preloader").style.display = "none";
-    }, 5200)
+    }, 600)
 })
